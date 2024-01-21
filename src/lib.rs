3aa362406511601a72d96479a4e2fbc44ae36b49
Vardhan Patil <hi@vardhanpatil.com>
2024-01-21 14:09:16 +0530

    Get basic tests working

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+pub mod io;
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[allow(dead_code)]
+pub fn test_runner(tests: &[&dyn crate::Testable]) {
+    serial_println!(" ******** Butterscotch OS 0.1.0 Alpha ********\n");
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+#[allow(dead_code)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+// Exits qemu, for testing without getting APM or ACPI working
+#[allow(dead_code, clippy::empty_loop)]
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+
+    serial_println!("Could not quit qemu! Make sure that tests are being run on qemu with the correct arguments");
+    loop {}
+}
+
+/// Panic handler for test mode
+pub fn test_panic_handler(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    test_panic_handler(info);
+}
