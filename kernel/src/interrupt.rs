c3d55620a5914f58e248c333d006f30bad2d4970
Saumya Patil <saumyasachinpatil@gmail.com>
2024-01-22 19:28:05 +0530

    Port memory allocation

--- kernel/src/interrupt.rs
+++ kernel/src/interrupt.rs
@@ -33,25 +33,8 @@ macro_rules! basic_handler {
 /// Should only be called on one thread, once
 /// Is called by main during startup
 pub unsafe fn init() {
-    _init(false)
-}
-
-/// Same as init, but provides a option to not panic, and instead
-/// exit with a success code
-///
-/// This is used in testing
-///
-/// # Safety
-///
-/// Should only be called on one thread, once
-/// Is called by main during startup
-pub unsafe fn _init(test_handler: bool) {
     IDT.double_fault
-        .set_handler_fn(if test_handler {
-            test_double_fault_handler
-        } else {
-            double_fault_handler
-        })
+        .set_handler_fn(double_fault_handler)
         .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
 
     IDT[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
@@ -134,19 +117,4 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
-}
-
-#[test_case]
-fn test_breakpoint_exception() {
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-}
-
-// Testing double fault handler
-extern "x86-interrupt" fn test_double_fault_handler(
-    _stack_frame: InterruptStackFrame,
-    _error_code: u64,
-) -> ! {
-    serial_println!("[ok]");
-    exit_qemu(QemuExitCode::Success);
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,70 +1,65 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
+#![feature(const_mut_refs)]
+#![feature(lazy_cell)]
 #![feature(custom_test_frameworks)]
 #![test_runner(butterscotch_kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-// extern crate alloc;
+extern crate alloc;
 
-// use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
-// use bootloader::{entry_point, BootInfo};
-use butterscotch_kernel::*;
+pub mod gdt;
+pub mod interrupt;
+pub mod io;
+pub mod kernel;
+pub use kernel::init;
+pub mod allocator;
+pub mod memory;
 
-// entry_point!(kernel_main);
-// static FRAMEBUFFER_REQUEST: limine::FramebufferRequest = limine::FramebufferRequest::new(0);
-/// Sets the base revision to 1, this is recommended as this is the latest base revision described
-/// by the Limine boot protocol specification. See specification for further info.
-static BASE_REVISION: limine::BaseRevision = limine::BaseRevision::new(1);
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    butterscotch_kernel::init();
+    init();
 
-    // // allocate a number on the heap
-    // let heap_value = Box::new(41);
-    // println!("heap_value at {:p}", heap_value);
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    serial_println!("heap_value at {:p}", heap_value);
 
-    // // create a dynamically sized vector
-    // let mut vec = Vec::new();
-    // for i in 0..500 {
-    //     vec.push(i);
-    // }
-    // println!("vec at {:p}", vec.as_slice());
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    serial_println!("vec at {:p}", vec.as_slice());
 
-    // // create a reference counted vector -> will be freed when count reaches 0
-    // let reference_counted = Rc::new(vec![1, 2, 3]);
-    // let cloned_reference = reference_counted.clone();
-    // println!(
-    //     "current reference count is {}",
-    //     Rc::strong_count(&cloned_reference)
-    // );
-    // core::mem::drop(reference_counted);
-    // println!(
-    //     "reference count is {} now",
-    //     Rc::strong_count(&cloned_reference)
-    // );
-
-    #[cfg(test)]
-    test_main();
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    serial_println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    serial_println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     eprintln!("Kernel did not crash");
 
     hlt_loop()
 }
 
-#[test_case]
-#[allow(clippy::eq_op)]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    test_panic_handler(info);
-}
-
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     eprintln!("{}", info);
