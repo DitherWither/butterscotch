bcccd81ae3bb6c8dd04400f3a1f30bab72602ee6
Saumya Patil <saumyasachinpatil@gmail.com>
2024-01-22 13:36:36 +0530

    Implement paging

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,32 @@
 #![test_runner(butterscotch_kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use butterscotch_kernel::*;
+use bootloader::{entry_point, BootInfo};
+use butterscotch_kernel::{memory, *};
+use x86_64::{
+    structures::paging::{FrameAllocator, Page, Translate},
+    VirtAddr,
+};
 
-#[no_mangle]
-#[allow(clippy::empty_loop)]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     butterscotch_kernel::init();
+
+    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(physical_memory_offset) };
+    let mut frame_allocator = unsafe {memory::BootInfoFrameAllocator::new(&boot_info.memory_map)};
+
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeef000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(100).write_volatile(0x_f021_f077_f065_f04e) };
+
     #[cfg(test)]
     test_main();
+
+    eprintln!("Kernel did not crash");
+
     hlt_loop()
 }
 
